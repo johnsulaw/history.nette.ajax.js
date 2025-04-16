764ccf722d599ae4ad2131efcf45859817a36850
johnsulaw <jan.sulavik@seznam.cz>
2025-04-16 12:27:38 +0200

    Fix nette/http

--- src/OnResponseHandler.php
+++ src/OnResponseHandler.php
@@ -65,8 +65,10 @@ class OnResponseHandler
 				if (($fragmentPos = strpos($payload->redirect, '#')) !== FALSE) {
 					$this->fragment = substr($payload->redirect, $fragmentPos);
 				}
-				$url = new Http\UrlScript($payload->redirect);
-				$url->setScriptPath($this->httpRequest->url->scriptPath);
+				$url = new Http\UrlScript(
+					$payload->redirect,
+					$this->httpRequest->url->scriptPath
+				);
 				$httpRequest = new Http\Request($url);
 
 				if ($this->router->match($httpRequest) !== NULL) {
